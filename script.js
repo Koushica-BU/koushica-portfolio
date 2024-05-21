16c0ad70e288e70bbc0946d87b26719bad2f9c8a
Koushica-BU <koushica01@gmail.com>
2024-05-21 16:00:59 -0500

    Updated Portfolio

--- script.js
+++ script.js
@@ -5,3 +5,17 @@ function toggleMenu(){
     menu.classList.toggle("open")
     icon.classList.toggle("open")
 }
+
+function openBackground(elem){
+    const backgroundMenu = document.querySelectorAll(".ImageContainer");
+    backgroundMenu.forEach(el => {
+        el.classList.remove('Active');
+    });
+    elem.classList.add('Active');
+    const details = document.querySelectorAll('.ContentContainer');
+    details.forEach(el => {
+        el.classList.remove('hide');
+    });
+    const hidedetails = document.querySelector(`.ContentContainer:not(#${elem.id}_details)`);
+    hidedetails.classList.add('hide')
+}
